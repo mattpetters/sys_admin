22800a818de168d70bf322012e823da4f4b63fae
Matt Petters <matt@petters.io>
2017-08-31 21:11:37 +0000

    adding git config, ufw config

---
+++ ubuntu/configs/configure_git.sh
@@ -0,0 +1,2 @@
+git config --global user.email "matt@petters.io"
+git config --global user.name "Matt Petters"
---
+++ ubuntu/configs/configure_ufw_nginx.sh
@@ -0,0 +1,4 @@
+sudo ufw app list
+sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 'Nginx HTTPS'
+sudo ufw status
